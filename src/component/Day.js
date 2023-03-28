c6462c4b48cb5267657619367d6af5932161eeab
Nick <nicholas00105@gmail.com>
2023-03-28 22:53:52 +0900

    Custome Hook : 비슷한 작업 묶어서 useFetch()로 만듬

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,22 +1,15 @@
-import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import useFetch from "./hooks/useFetch.js";
 import Word from "./Word.js"
 export default function Day(){
     
     
     const day = useParams().day;
     
-    const [words, setWords] = useState([]);
+
+    const words = useFetch(`http://localhost:3001/words?day=${day}`)
     
-    useEffect(() => {
-        fetch(`http://localhost:3001/words?day=${day}`)
-        .then(res => {
-            return res.json()
-        })
-        .then(data => {
-            setWords(data)
-        });
-    }, [day])
+
     return ( <>
         <h2>Day {day} </h2>
         <table>
